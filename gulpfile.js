d3822e6284055fe383efe389ee599e7a71e90bca
= <igorthurow@gmail.com>
2018-08-19 19:56:32 -0300

    Prepara para produção. Minifica html, less e images.

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,30 @@ var gulp = require('gulp'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
     livereload = require ('gulp-livereload');
+    htmlmin = require('gulp-htmlmin');
+    image = require('gulp-image');
+ 
+gulp.task('image', function () {
+  gulp.src('./src/resources/*')
+    .pipe(image())
+    .pipe(gulp.dest('./app/resources'));
+});
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('./css/*.less', ['less']);
-  gulp.watch('./*.html', ['html']);
+  gulp.watch('./src/*.less', ['less']);
+  gulp.watch('./src/*.html', ['html']);
 });
 
 gulp.task('html', function() {
-  gulp.src('./*.html')
+  gulp.src('./src/*.html')
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('./app/'))
   .pipe(livereload());
 });
 
 gulp.task('less', function () {
-  return gulp.src('./css/*.less')
+  return gulp.src('./src/*.less')
     .pipe(less().on('error', function (err) {
       console.log(err);
     }))
@@ -26,6 +36,6 @@ gulp.task('less', function () {
     .pipe(livereload().on('error', function (err) {
       console.log(err); }))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./app/'));
 });
-gulp.task('default', ['less', 'html', 'watch']);
+gulp.task('default', ['less', 'html', 'watch', 'image']);
